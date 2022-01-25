99613eef1289222a7e261f8b0c629c99351e73df
Caio042 <caiohenriquegomeslima@gmail.com>
2022-01-25 18:49:20 -0300

    Initial commit

---
+++ src/main/java/com/caiolima/bcontrol/BcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.caiolima.bcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiolima/bcontrol/BcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiolima.bcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
