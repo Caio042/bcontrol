385cdb0574e1b2e347215309513007de6cae3159
Caio042 <caiohenriquegomeslima@gmail.com>
2022-06-18 14:32:11 -0300

    Retira union

--- src/main/java/com/caiolima/bcontrol/repository/RegistroFinanceiroRepository.java
+++ src/main/java/com/caiolima/bcontrol/repository/RegistroFinanceiroRepository.java
@@ -10,11 +10,7 @@ import java.util.List;
 public interface RegistroFinanceiroRepository extends JpaRepository<RegistroFinanceiro, Long> {
     @Query(value = "SELECT DISTINCT " +
             "new java.lang.Integer(YEAR(r.data)) " +
-            "FROM Receita r " +
-            "WHERE r.usuario.email = :email " +
-            "UNION " +
-            "SELECT new java.lang.Integer(YEAR(d.data)) " +
-            "FROM Despesa d " +
-            "WHERE  d.usuario.email")
+            "FROM RegistroFinanceiro r " +
+            "WHERE r.usuario.email = :email")
     List<Integer> getYears(@Param("email") String email);
 }
